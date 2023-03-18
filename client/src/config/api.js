069071322b501eb765ddde297f5016e9538c1926
harumi <firmansyah720.fs@gmail.com>
2023-03-18 12:43:45 +0700

    change base url client

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-    baseURL: 'http://localhost:5000/api/v1/',
+    baseURL: 'http://ec2-108-137-76-240.ap-southeast-3.compute.amazonaws.com:5000/api/v1/',
 });
 
 export const setAuthToken = (token) => {
